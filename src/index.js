47d3e3be85775b86ed37f67a1b94ff943f02fd5f
Q-Zhao <qzhao@wpi.edu>
2017-10-09 21:48:25 -0400

    add earth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+//import detailHandler from './detailHandler'
 
-var width = 600,
+var width = 1600,
     height = 500,
     sens = 0.25,
     focused;
@@ -9,27 +10,30 @@ var width = 600,
 var projection = d3.geo.orthographic()
     .scale(245)
     .rotate([0, 0])
-    .translate([width / 2, height / 2])
+    .translate([width * 1/5, height / 2])
     .clipAngle(90);
 
 var path = d3.geo.path()
     .projection(projection);
 
 //SVG container
+var earthPanel = document.getElementById("earthPanel");
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select(earthPanel).append("svg")
     .attr("width", width)
     .attr("height", height);
 
 //Adding water
 
-svg.append("path")
+svg.append("g").attr("class", "group0")
+    .append("path")
     .datum({type: "Sphere"})
     .attr("class", "water")
     .attr("d", path);
 
-var countryTooltip = d3.select("body").append("div").attr("class", "countryTooltip"),
-    countryList = d3.select("body").append("select").attr("name", "countries");
+var countryTooltip = d3.select(earthPanel).append("div").attr("class", "countryTooltip");
+var countrySelector = document.getElementById("countrySelector");
+var countryList = d3.select(countrySelector).append("select").attr("name", "countries");
 
 
 queue()
@@ -46,7 +50,7 @@ function ready(error, world, countryData) {
 
     //Adding countries to select
 
-    countryData.forEach(function(d) {
+    countryData.forEach(function (d) {
         countryById[d.id] = d.name;
         option = countryList.append("option");
         option.text(d.name);
@@ -64,8 +68,11 @@ function ready(error, world, countryData) {
         //Drag event
 
         .call(d3.behavior.drag()
-            .origin(function() { var r = projection.rotate(); return {x: r[0] / sens, y: -r[1] / sens}; })
-            .on("drag", function() {
+            .origin(function () {
+                var r = projection.rotate();
+                return {x: r[0] / sens, y: -r[1] / sens};
+            })
+            .on("drag", function () {
                 var rotate = projection.rotate();
                 projection.rotate([d3.event.x * sens, -d3.event.y * sens, rotate[2]]);
                 svg.selectAll("path.land").attr("d", path);
@@ -73,26 +80,30 @@ function ready(error, world, countryData) {
             }))
 
         //Mouse events
+        .on("dblclick", function (d) {
+            document.getElementById("detailsTitle").innerHTML = "Detailed Information of " + countryById[d.id];
+            updateDetail(countryById[d.id]);
+        })
 
-        .on("mouseover", function(d) {
+        .on("mouseover", function (d) {
             countryTooltip.text(countryById[d.id])
                 .style("left", (d3.event.pageX + 7) + "px")
                 .style("top", (d3.event.pageY - 15) + "px")
                 .style("display", "block")
                 .style("opacity", 1);
         })
-        .on("mouseout", function(d) {
+        .on("mouseout", function (d) {
             countryTooltip.style("opacity", 0)
                 .style("display", "none");
         })
-        .on("mousemove", function(d) {
+        .on("mousemove", function (d) {
             countryTooltip.style("left", (d3.event.pageX + 7) + "px")
                 .style("top", (d3.event.pageY - 15) + "px");
         });
 
     //Country focus on option select
 
-    d3.select("select").on("change", function() {
+    d3.select("select").on("change", function () {
         var rotate = projection.rotate(),
             focusedCountry = country(countries, this),
             p = d3.geo.centroid(focusedCountry);
@@ -104,21 +115,111 @@ function ready(error, world, countryData) {
         (function transition() {
             d3.transition()
                 .duration(2500)
-                .tween("rotate", function() {
+                .tween("rotate", function () {
                     var r = d3.interpolate(projection.rotate(), [-p[0], -p[1]]);
-                    return function(t) {
+                    return function (t) {
                         projection.rotate(r(t));
                         svg.selectAll("path").attr("d", path)
-                            .classed("focused", function(d, i) { return d.id == focusedCountry.id ? focused = d : false; });
+                            .classed("focused", function (d, i) {
+                                return d.id == focusedCountry.id ? focused = d : false;
+                            });
                     };
                 })
         })();
     });
 
     function country(cnt, sel) {
-        for(var i = 0, l = cnt.length; i < l; i++) {
-            if(cnt[i].id == sel.value) {return cnt[i];}
+        for (var i = 0, l = cnt.length; i < l; i++) {
+            if (cnt[i].id == sel.value) {
+                return cnt[i];
+            }
+        }
+    };
+};
+
+
+function updateDetail(countryName){
+    //var chartDiv = document.getElementById("chart");
+    //svg = d3.select(chartDiv).append("svg");
+    const xValue = d => d['Year'];
+    const xLabel = 'Year';
+    const yValue = d => d['Mobile cellular subscriptions'];
+    const yLabel = 'Subscription per 100 people';
+
+    var innerWidth = width/2;
+    var innerHeight = height;
+
+    //var g = svg.selectAll("*").exit().append('g');
+    var g = svg.append("g");
+    const xAxisG = g.append('g')
+        .attr('transform', `translate(0, ${innerHeight})`);
+    const yAxisG = g.append('g');
+
+    xAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('x', innerWidth / 2)
+        .attr('y', 80)
+        .text(xLabel);
+
+    yAxisG.append('text')
+        .attr('class', 'axis-label')
+        .attr('x', -innerHeight / 2)
+        .attr('y', -60)
+        .attr('transform', `rotate(-90)`)
+        .style('text-anchor', 'middle')
+        .text(yLabel);
+
+    const xScale = d3.scale.linear();
+    const yScale = d3.scale.linear();
+
+    const xAxis = g.axis()
+        .scale(xScale)
+        .tickPadding(15)
+        .ticks(5)
+        .tickSize(-innerHeight)
+        .tickFormat(d3.format("d"));
+
+    const yTicks = 5;
+    const yAxis = g.axis()
+        .scale(yScale)
+        .ticks(yTicks)
+        .tickPadding(15)
+        .tickSize(-innerWidth)
+        .tickFormat(d3.format("d"));
+
+    const line = g.line()
+        .x(d => xScale(xValue(d)))
+        .y(d => yScale(yValue(d)))
+        .interpolate("basis");
+
+    const row = d => {
+        if(d['Entity'] === countryName){
+            d['Year'] = +d['Year'];
+            d['Mobile cellular subscriptions'] = +d['Mobile cellular subscriptions'];
+            return d;
         }
     };
 
-};
+    d3.csv('/data/mobile-cellular-subscriptions-by-country.csv', row, data => {
+        xScale
+            .domain(d3.extent(data, xValue))
+            .range([0, innerWidth])
+            .nice();
+
+        yScale
+            .domain(d3.extent(data, yValue))
+            .range([innerHeight, 0])
+            .nice(yTicks);
+
+        g.append('path')
+            .attr('fill', 'none')
+            .attr('stroke', 'red')
+            .attr('stroke-width', 4)
+            .attr('d', line(data));
+
+        xAxisG.call(xAxis);
+        yAxisG.call(yAxis);
+    });
+}
+
+
