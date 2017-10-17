11a47477a90a91f67fa5066830520f853f8f0ebf
Jose Miguel Pasini <josemiguelpasini@gmail.com>
2017-10-17 01:10:15 -0400

    fully resizable calendar

--- src/calendar.js
+++ src/calendar.js
@@ -17,119 +17,165 @@ function calendar(container, props, box) {
 
   const width = box.width,
     height = box.height,
-    cellSize = d3.min([width/(nWeeks+10), height/(nDays+8)]);
+    cellSize = d3.min([width/(nWeeks+12), height/(nDays+4)]);
 
   
+  const legendColors = ['#fff', '#ffffb2', '#fecc5c', '#fd8d3c', '#f03b20', '#bd0026'];
+  const legendLabels = [null, '1 to 5', '6 to 10', '11 to 15', '16 to 20', 'over 20'];
   const color = d3.scaleThreshold()
       .domain([1, 6, 11, 16, 21])
-      .range(['#fff', '#ffffb2', "#fecc5c", "#fd8d3c", "#f03b20", "#bd0026"]);
-  const legendLabels = [null, '1 to 5', '6 to 10', '11 to 15', '16 to 20', 'over 20'];
+      .range(legendColors);
 
   const currentYear = 2017;
 
   // use the "manage only one thing" GUP
   // Calendar group
-  const calendarG = container.selectAll('.calendargroup').data([null])
+  let calendarG = container.selectAll('.calendargroup').data([null]);
+  const calendarEnter = calendarG
     .enter().append('g')
-      .attr('class', 'calendargroup')
+      .attr('class', 'calendargroup');
+  calendarG = calendarEnter.merge(calendarG)
       .attr("transform", "translate(" + 
         ((width - cellSize * 53) / 2 - 2*cellSize) + "," + 
         (height - cellSize * 7 - 1)/2 + ")");
 
   // year label
-  calendarG.append("text")
+  const yearLabel = calendarG.selectAll('.yearLabel').data([null]);
+  yearLabel
+    .enter()
+    .append("text")
       .attr("class", "yearLabel")
       .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
-      .attr("font-family", "sans-serif")
       .attr("font-size", cellSize*1.8)
       .attr("text-anchor", "middle")
-      .text(currentYear);
+      .text(currentYear)
+    .merge(yearLabel)
+      .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+      .attr("font-size", cellSize*1.8);
+
+  const data = d3.nest()
+      .key(d => d.DateString)
+      .rollup(d => { return {"length": d.length, "races": d.map(x => x.Town + " (" + x.Distance + "): " +  x.Name).sort().join("\n")}; })
+    .object(racesData);
 
   // draw the background grid
   // Note: this relies on the top-left corner of this group being (0,0)
-  const rect = calendarG.append("g")
-      .attr("fill", "none")
-    .selectAll("rect")
-    .data(d3.timeDays(new Date(currentYear, 0, 1), new Date(currentYear + 1, 0, 1)))
+  let rect = calendarG
+    .selectAll('rect')
+    .data(d3.timeDays(new Date(currentYear, 0, 1), new Date(currentYear + 1, 0, 1)));
+
+  rect = rect
     .enter().append("rect")
+      .attr('fill', 'none')
       .attr("width", cellSize)
       .attr("height", cellSize)
       .attr("x", d => d3.timeWeek.count(d3.timeYear(d), d) * cellSize)
       .attr("y", d => d.getDay() * cellSize)
       .attr("stroke", "#ccc")
       .attr("stroke-width", 1)
-      .datum(d3.timeFormat("%Y-%m-%d"));
+    .merge(rect)
+      .attr("width", cellSize)
+      .attr("height", cellSize)
+      .attr("x", d => d3.timeWeek.count(d3.timeYear(d), d) * cellSize)
+      .attr("y", d => d.getDay() * cellSize);
 
-    const data = d3.nest()
-        .key(d => d.DateString)
-        .rollup(d => { return {"length": d.length, "races": d.map(x => x.Town + " (" + x.Distance + "): " +  x.Name).sort().join("\n")}; })
-      .object(racesData);
+  // fill the rects for each day
+  const fmt2 = d3.timeFormat("%Y-%m-%d");
+  rect.filter(d => fmt2(d) in data)
+      .attr("fill", d => color(data[fmt2(d)].length))
+      .attr("class", "day_with_race")
+    .append("title")
+      .text(d => fmt2(d) + ": " + formatCell(data[fmt2(d)].length) + " races\n" +  data[fmt2(d)].races);
 
-    const legend = d3.legendColor()
-      .labelFormat(d3.format("0"))
-      .cells(4)
-      .scale(color)
-      .orient('vertical')
-      .shapeWidth(cellSize)
-      .shapeHeight(cellSize)
-      .labels(legendLabels)
-      .title('Races');
+  // draw the color legend manually
+  // use the "manage only one thing" version of the General Update Pattern
+  let colorLegendG = calendarG.selectAll('.calendarLegendG').data([null]);
+  colorLegendG = colorLegendG
+    .enter().append('g')
+      .attr('class', 'calendarLegendG')
+    .merge(colorLegendG)
+      .attr("transform", "translate(" + (54*cellSize) + "," + "0)");
 
-    // fill the rects for each day
-    rect.filter(d => d in data)
-        .attr("fill", d => color(data[d].length))
-        .attr("class", "day_with_race")
-      .append("title")
-        .text(d => d + ": " + formatCell(data[d].length) + " races\n" +  data[d].races);
-
-    // use the "manage only one thing" version of the General Update Pattern
-    const colorLegendG = calendarG.selectAll('.legend').data([null])
-      .enter().append('g')
-      .attr("transform", "translate(" + (54*cellSize) + "," + (-1*cellSize) + ")");
-
-    colorLegendG.call(legend)
-        .attr('class', 'legend');
-
-  calendarG.append("g")
-      .attr("id", "monthOutlines")
-      .attr("fill", "none")
-      .attr("stroke", "#666")
-      .attr("stroke-width", "2")
-    .selectAll("path")
-    .data(d3.timeMonths(new Date(currentYear, 0, 1), new Date(currentYear + 1, 0, 1)))
-    .enter().append("path")
-      .attr("d", pathMonth);
+  const colorLegend = colorLegendG.selectAll('rect').data(legendColors.slice(1));
+  const legendLineHeight = cellSize*1.4;
+  colorLegend
+    .enter().append('rect')
+      .attr('x', 0)
+    .merge(colorLegend)
+      .attr('fill', d => d)
+      .attr('width', legendLineHeight*.9)
+      .attr('height', legendLineHeight*.9)
+      .attr('y', (d, i) => (i-0.5)*legendLineHeight);
+  
+  const colorLegendText = colorLegendG.selectAll('text').data(legendLabels.slice(1));
+  colorLegendText
+    .enter().append('text')
+      .attr('fill', d => d)
+      .attr('fill', '#666')
+      .attr('text-anchor', 'middle')
+      .attr('alignment-baseline', 'middle')
+      .text(d => d)
+    .merge(colorLegendText)
+      .attr('font-size', cellSize)
+      .attr('x', cellSize*4)
+      .attr('y', (d, i) => i*(legendLineHeight));
 
   // frame for today's date
   const today = d3.timeDay(new Date());
-  const todayRect = calendarG.append("rect")
+  const todayRect = calendarG.selectAll('.todayDate').data([null]);
+  todayRect
+    .enter().append("rect")
+      .attr('class', 'todayDate')
       .attr("fill", "none")
       .attr("width", cellSize)
       .attr("height", cellSize)
       .attr("x", d3.timeWeek.count(d3.timeYear(today), today)*cellSize)
       .attr("y", today.getDay() * cellSize)
       .attr("stroke", "darkred")
-      .attr("stroke-width", 3);
+      .attr("stroke-width", 3)
+    .merge(todayRect)
+      .attr("width", cellSize)
+      .attr("height", cellSize)
+      .attr("x", d3.timeWeek.count(d3.timeYear(today), today)*cellSize)
+      .attr("y", today.getDay() * cellSize);
 
+  // monthOutlines
+  let monthOutlinesG = calendarG.selectAll('#monthOutlines').data([null]);
+  monthOutlinesG = monthOutlinesG
+    .enter().append('g')
+      .attr('id', 'monthOutlines')
+      .attr('fill', 'none')
+      .attr('stroke', '#666')
+      .attr('stroke-width', '2')
+    .merge(monthOutlinesG);
 
-  // ideas for month/day labels:
-  // https://bl.ocks.org/alansmithy/6fd2625d3ba2b6c9ad48
+  const monthOutlines = monthOutlinesG.selectAll('.monthPath')
+    .data(d3.timeMonths(new Date(currentYear, 0, 1), new Date(currentYear + 1, 0, 1)));
+  monthOutlines
+    .enter().append('path')
+      .attr('class', 'monthPath')
+      .attr('d', pathMonth)
+    .merge(monthOutlines)
+      .attr('d', pathMonth);
+
 
   // get bounding box for each month outline
   const mp = document.getElementById("monthOutlines").childNodes;
-  const BB = Object.values(mp).map(d => d.getBBox());
+  const BB = Array.prototype.slice.call(mp).map(d => d.getBBox());
   const monthX = BB.map(d => d.x + d.width/2);
   // add the labels
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-  const monthLabels = calendarG.append("g").attr("id", "monthLabels");
-  months.forEach((d, i) => {
-    monthLabels.append("text")
-        .attr("class", "monthLabel")
-        .attr("x", monthX[i])
-        .attr("y", -10)
-        .attr('font-size', cellSize*1.2)
-        .text(d);
-  });
+  const monthLabels = calendarG.selectAll('.monthLabel').data(months);
+  monthLabels
+    .enter().append('text')
+      .attr('class', 'monthLabel')
+      .attr('x', (d,i) => monthX[i])
+      .attr('y', -10)
+      .attr('font-size', cellSize*1.2)
+      .text(d => d)
+    .merge(monthLabels)
+      .attr('x', (d,i) => monthX[i])
+      .attr('font-size', cellSize*1.2);
 
   function pathMonth(t0) {
     const t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
@@ -141,8 +187,6 @@ function calendar(container, props, box) {
         + "H" + (w1 + 1) * cellSize + "V" + 0
         + "H" + (w0 + 1) * cellSize + "Z";
   }
-
-
 }
 
 export { calendar, parseRace };
