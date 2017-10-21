d3b3511ae48a4a46ac35a0c7b212f6bf84d60bad
sajudson <sajudson@gmail.com>
2017-10-21 16:37:42 -0400

    update radial.js based on blocks template prototype
    added x axis ticks and labels
    added y axis tick marks
    added y axis label

--- dist/bundle.js
+++ dist/bundle.js
@@ -650,21 +650,6 @@ const colorScale = d3.scaleOrdinal()
   .range(d3.schemeCategory10);
 
 
-const xAxis = d3.axisBottom()
-  .scale(xScale)
-  .tickPadding(10)
-  .tickFormat(d3.format('0'))
-  .tickSize(-innerHeight);
-
-const yAxis = d3.axisLeft()
-  .scale(yScale)
-  .tickFormat(d3.format('.2s'))
-  .tickPadding(10)
-  .tickSize(-innerWidth);
-
-const colorLegend = d3.legendColor()
-  .scale(colorScale)
-  .shape('circle');
 
 /* unused harmony default export */ var _unused_webpack_default_export = (function (div, props) {
   const {
@@ -713,102 +698,147 @@ const colorLegend = d3.legendColor()
     .range([0, 2*Math.pi]);
 
   yScale
-    .domain(d3.extent(data, yValue)) //d3.extent(data, yValue
-    .range([innerHeight/2, 0])
+    .domain([0,1000]) //d3.extent(data, yValue
+    .range([0,innerHeight/2])
     .nice();
 
   var g = svg.selectAll('g').data([null]);
 
-  g = g.enter().append('g')
-    .merge(g)
-    .attr('transform', `translate(${innerWidth/2},${innerHeight/2})`);
+  // translate origin to center of inner svg
+        const g = svg.append('g')
+            .attr('transform', `translate(${innerWidth/2+margin.left},${innerHeight/2+margin.top})`);
+
+  //draw ticklines
+        const yMax = 1000;
+        const yScaleMax = innerHeight/2;
+        const numXTicks =24;
+        const numYTicks =4;
+        const yScaleradialScaleFactor = (innerHeight/2*yScaleMax);
+
+        const drawYTicks = function(yScaleMax, numYTicks) {
+          for (i=0; i<numYTicks; i++){
+            g.append('circle')
+          		.attr('cx',0)
+          		.attr('cy',0)
+              .attr('fill', 'none')
+              .attr('stroke', 'lightgrey')
+              .attr('stroke-width', 1)
+              .attr('r', yScaleMax * (i+1)/numYTicks)
+          };
+        };
+
+        const drawXTicks = function (numXTicks){
+
+          const tickEndPoint = function(angle,radius){
+            x = radius * Math.cos(angle)
+            y = radius * Math.sin(angle)
+            return [x,y]};
+
+          const tickAngles = function(numberOfTicks){
+              let tickAngles = []
+              for(i=0; i<numberOfTicks; i++){
+                angle = Math.PI *2 * i/numberOfTicks
+                tickAngles.push(angle)        }
+              return tickAngles};
+
+          const tickCoordinates = function(numberOfTicks,tickRadius){
+              let tickAngle = tickAngles(numberOfTicks)
+              let xTickArray =[]
+              for (i = 0; i <tickAngle.length; i++){
+                xy = tickEndPoint(tickAngle[i],tickRadius)
+                xTickArray.push(xy)
+              }
+              return xTickArray
+             };
+
+          let t = tickAngles(numXTicks);
+          let xTickLength = yScaleMax *1.05;
+          let xTickArray = tickCoordinates (numTicks,xTickLength);
+
+          for (i in xTickArray){
+            g.append('line')
+          		.attr('x1',0)
+          		.attr('y1',0)
+              .attr('x2',xTickArray[i][0])
+          		.attr('y2',xTickArray[i][1])
+              .attr('fill', 'none')
+              .attr('stroke', 'lightgrey')
+              .attr('stroke-width', 1);
+
+            g.append('text')
+            	.attr('class', "tick text")
+            	.text("xtick")
+              .attr('text-anchor', 'middle')
+              .attr("x", xTickArray[i][0])
+              .attr("y",xTickArray[i][1])
+          .attr('transform',`rotate(${90+(t[i])*360/(2*Math.PI)},${xTickArray[i][0]},${xTickArray[i][1]})`)
+              .attr('font-family', 'sans-serif')
+              .attr('fill',"grey")
+            	.attr('font-size', 11)
+              .text(`${i/numTicks*2400}`)
+          };
+        }
+
+        //draw x abd y tick marks and labels
+        drawXTicks(numXTicks);
+        drawYTicks(yScaleMax, numYTicks);
+
+        yAxisG.append('text')
+            .attr('class', 'axis-label')
+            .attr('x', +innerHeight / 4+5)
+            .attr('y', -5)
+            .attr('transform', `rotate(0)`)
+            .style('text-anchor', 'middle')
+            .text(yLabel);
+
+        const xScale = d3.scaleTime();
+        const yScaleLeft = d3.scaleLinear()
+          .domain([0,yMax])
+          .range([0,yScaleMax]);
+
+        const colorUser = {Total:"grey", Casual:"red",  Registered:"green"}
+
+        const minDate = new Date(2012,1,1)
+        const centerOffset = innerHeight/2;
+        const radialOffset = 0 //.25*Math.PI
+
+        const angleHours = d => (xValue(d)/24 *Math.PI*2+ radialOffset);
+        var plotColor = colorUser.Registered
 
 
 
+        const radialTimeSeriesLine = d3.lineRadial()
+          .angle(d => angleHours(d))
+          .radius(d => yValue(d)*radialScaleFactor)
+          .curve(d3.curveLinear);
 
-  //data join
-  var line = g.selectAll('path').data(data);
+        //data join
+        const radialPath = g.selectAll('path').data(data);
 
-  //Add new elements
-  var lineEnter = line.enter().append('path');
+        //Add new elements
+        var radialPathEnter = radialPath.enter().append('path');
 
-  var circlesExit = line.exit()
-    .attr('class','exit')
-    .remove();
+        var radialPathExit = radialPath.exit()
+            .attr('class','exit')
+            .remove();
 
-  //UPDATE old elements present (change class)
-  line
-    .attr('class','update');
+        //UPDATE old elements present (change class)
+        radialPath
+            .attr('class','update');
 
-  //merge new and existing ell
-  circlesEnter
-    .attr('class','enter')
-    .attr('fill', colorValue)
-    .attr('fill-opacity', .2)
-    .attr('r', pointSize)
-   .merge(circles)
-    .attr('cx', d => xScale(xValue(d)))
-    .attr('cy', d => yScale(yValue(d)));
+        //merge new and existing ell
+        radialPathEnter
+          .attr('class','enter')
+          .merge(radialPath)
+          .attr('fill', 'none')
+          .attr('stroke', colorValue)
+          .attr('stroke-opacity', 0.7)
+          .attr('stroke-width', .1)
+          .attr('d', radialTimeSeriesLine(data));
 
-  //remove elements for which there is no data
-  circlesExit
-
-  //call X and Y axis
-  xAxisG.call(xAxis);
-  yAxisG.call(yAxis);
-
-  colorLegendG.call(colorLegend)
-    .selectAll('.cell text')
-    .attr('dy', '0.1em');
-
-
-// TODO adapt radial time series code to
-// const xValue = d => d.dteday;
-
-
-
-//       function deltaDays(date1, date2){
-//         const dayms = 1000 * 60 * 60 * 24;
-//         var date1ms = date1.geTime();
-//         var date2ms = date2.getTime();
-//         var deltams = date1ms-date2ms
-//         return (deltams/dayms)
-//         };
-
-       const colorUser = {Total:"grey", Casual:"red",  Registered:"green"}
-
-      const minDate = new Date(2012,1,1);
-      const msPerHour = (1000*60*60);
-      const msPerDay = msPerHour * 24;
-      const hoursSinceStart = d => ((d.dteday.getTime() - minDate.getTime())/msPerHour);
-      var plotColor = colorUser.Registered;
-//
-      const line = d3.lineRadial()
-        .angle(d => xScale(hoursSinceStart(d)))
-        .radius(d => yScale(yValue(d)))
-        .curve(d3.curveBasis);
-
-        g.append('path')
-            .attr('fill', 'none')
-            .attr('stroke', plotColor)
-            .attr('stroke-opacity', 0.7)
-            .attr('stroke-width', .1)
-            .attr('d', line(data));
-
-        g.append('circle')
-            .attr('cx',innerWidth/2)
-            .attr('cy',innerHeight/2)
-            .attr('fill', 'none')
-            .attr('stroke', 'grey')
-            .attr('stroke-width', 2)
-            .attr('r', innerHeight/2);
-
-
-
-
-
-
-    });;
+      //remove elements for which there is no data
+      radialPathExit
+  });;
 
 
 /***/ })
